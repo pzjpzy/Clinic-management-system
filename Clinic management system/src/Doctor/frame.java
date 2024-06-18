85a815f3b3ca1660a60e6461e116cd1e32491931
pzjpzy <pangzaijie@gmail.com>
2024-06-18 22:13:44 +0800

    admin delete

--- Clinic management system/src/Doctor/frame.java
+++ Clinic management system/src/Doctor/frame.java
@@ -103,12 +103,20 @@ public class frame extends JFrame implements ActionListener {
             // Remove all components from the content pane
             getContentPane().removeAll();
             //add all components back
-            getContentPane().add(new patientmenuPanel());
+            getContentPane().add(new NewJPanel());
             // Revalidate and repaint to update the UI
             getContentPane().revalidate();
             getContentPane().repaint();
             
             }
         }
-                  
+        public void switchpanel(JPanel pane){
+            // Remove all components from the content pane
+            getContentPane().removeAll();
+            //add all components back
+            getContentPane().add(pane);
+            // Revalidate and repaint to update the UI
+            getContentPane().revalidate();
+            getContentPane().repaint();
+        }        
    }
--- Clinic management system/src/Patient/Main.java
+++ Clinic management system/src/Patient/Main.java
@@ -14,14 +14,13 @@ import Doctor.frame;
 public class Main {
     public static void main(String[] args) {
 //    	patientMenu main = new patientMenu();
-<<<<<<< HEAD
-//        Appointment ap = new Appointment();
-          nAppointment nap = new nAppointment();
-//        NewClass newClass = new NewClass();
-=======
-//        NewClass newClass = new NewClass("Admin");
         frame frame = new frame();
->>>>>>> 746c11b8859bd30e4107065471b7a161d46a630a
+//        Appointment ap = new Appointment();
+//          nAppointment nap = new nAppointment();
+//        NewClass newClass = new NewClass();
+
+//        NewClass newClass = new NewClass("Admin");
+        
         
     }
     

a21699bde3fb692cf1508d1803af60409c504939
ZaiYong36 <139421497+ZaiYong36@users.noreply.github.com>
2024-06-18 23:15:53 +0800

    update

--- Clinic management system/src/Patient/Main.java
+++ Clinic management system/src/Patient/Main.java
@@ -14,14 +14,15 @@ import Doctor.frame;
 public class Main {
     public static void main(String[] args) {
 //    	patientMenu main = new patientMenu();
-<<<<<<< HEAD
-//        Appointment ap = new Appointment();
-          nAppointment nap = new nAppointment();
+
+//          Appointment ap = new Appointment();
+//          nAppointment nap = new nAppointment();
 //        NewClass newClass = new NewClass();
-=======
+//            test t = new test();
+            test2 t2 = new test2();
 //        NewClass newClass = new NewClass("Admin");
-        frame frame = new frame();
->>>>>>> 746c11b8859bd30e4107065471b7a161d46a630a
+//        frame frame = new frame();
+
         
     }
     
